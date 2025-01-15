891cdd691e6ab5475db209037eddb56d1ac3215e
Marc Chevrière <123384932+marc-chevriere@users.noreply.github.com>
2025-01-15 20:38:05 +0100

    fix again

--- autoencoder.py
+++ autoencoder.py
@@ -155,8 +155,8 @@ class VariationalAutoEncoder(nn.Module):
         self.fc_mu = nn.Linear(hidden_dim_enc, latent_dim)
         self.fc_logvar = nn.Linear(hidden_dim_enc, latent_dim)
         # self.decoder = Decoder(latent_dim*2, hidden_dim_dec, n_layers_dec, n_max_nodes)
-        self.decoder = RNNDecoderwAtt(latent_dim=latent_dim*2, hidden_dim=hidden_dim_dec, n_layers=n_layers_dec, max_nodes=n_max_nodes)
-        # self.decoder = TransformerDecoderModel(latent_dim=latent_dim*2, hidden_dim=hidden_dim_dec, n_layers=n_layers_dec, max_nodes=n_max_nodes)
+        # self.decoder = RNNDecoderwAtt(latent_dim=latent_dim*2, hidden_dim=hidden_dim_dec, n_layers=n_layers_dec, max_nodes=n_max_nodes)
+        self.decoder = TransformerDecoderModel(latent_dim=latent_dim*2, hidden_dim=hidden_dim_dec, n_layers=n_layers_dec, max_nodes=n_max_nodes)
         self.cond_encoder = condEncoder(cond_dim=7,latent_dim=latent_dim)
         self.lambda_contrastive = lambda_contrastive
         self.beta = beta
